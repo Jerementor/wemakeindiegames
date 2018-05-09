9f8603596d28dfcb9b169ca1a1398f5dcb36cd4d
Jeremy Alexander <jwalexander22@gmail.com>
2018-05-09 08:15:00 +0000

    put nav to header

--- header.php
+++ header.php
@@ -50,7 +50,11 @@
 
 <body>
 
-
+<?php if (is_user_logged_in() ) : ?>
+<?php get_template_part('templates/navbar/logged_in_nav'); ?>
+<?php else: ?>
+<?php get_template_part('templates/navbar/logged_out_nav'); ?>
+<?php endif; ?>   
  
 
 

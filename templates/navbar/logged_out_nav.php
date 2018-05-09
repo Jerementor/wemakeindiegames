7be337d8962845989f5240f498a1d3146a44b1ac
Jeremy Alexander <jwalexander22@gmail.com>
2018-05-09 07:58:36 +0000

    navbar brand

--- templates/navbar/logged_out_nav.php
+++ templates/navbar/logged_out_nav.php
@@ -1,7 +1,7 @@
 <div data-collapse="medium" data-animation="default" data-duration="400" class="navbar w-nav">
 <div class="w-container">
   <a href="/" class="w-nav-brand">
-    <div class="text-block">We Make Indie Games (C) 2018</div>
+    <div class="text-block">We Make Indie Games</div>
   </a>
   <nav role="navigation" class="nav-menu w-nav-menu">
  <?php get_template_part( 'templates/navbar/nav_out'); ?>    
